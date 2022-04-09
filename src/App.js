410e35eca472647494680dda1517894dc8591983
anwarul1 <anwarjewel007@gmail.com>
2022-04-09 06:02:51 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import './App.css';
+import {
+  getAuth,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
+// import firebaseApp, { auth } from "./firebase.init";
+import app from './firebase.init';
+import { useState } from 'react';
+
+const auth = getAuth(app);
+
+function App() {
+  const [user, setUser] = useState({});
+
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        // console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleSignOut = () => {
+    //
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch((error) => {
+        setUser({});
+      });
+  };
+
+  return (
+    <div className="App">
+      {/* { condition ? true : false } */}
+
+      {user.uid ? (
+        // if email does have
+        <button onClick={handleSignOut}>Sign out</button>
+      ) : (
+        // if email does not have
+        <>
+          <button onClick={handleGoogleSignIn}>Google Sign In</button>
+          <button onClick={handleGithubSignIn}>Github Sign In</button>
+        </>
+      )}
+      <h2>Name: {user.displayName}</h2>
+      <p>I know your Email: {user.email}</p>
+      <img src={user.photoURL} alt="" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyD21-rtu9nDtoCCsJfLa010-O5xPzRcRGs',
+  authDomain: 'simple-firebase-authenti-79bea.firebaseapp.com',
+  projectId: 'simple-firebase-authenti-79bea',
+  storageBucket: 'simple-firebase-authenti-79bea.appspot.com',
+  messagingSenderId: '311598678520',
+  appId: '1:311598678520:web:7ca780c21c50daa1b7d60d',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
